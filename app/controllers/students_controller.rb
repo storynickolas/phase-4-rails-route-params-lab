e5ba55edbb18fe27ab703e3b907c4701eeccbf32
Nick Story <storynickolasl@gmail.com>
2023-03-22 21:16:15 -0400

    commit tree d89a88724eaf8b69a06786e36d500ecb12af70d0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:first_name] || params[:last_name]
+      student = Student.where(:last_name => params[:last_name]).or(Student.where(:first_name => params[:first_name]))
+      render json: student
+    else
+      student = Student.all
+      render json: student
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def sample
+    student = Student.where(:last_name => params[:last_name])
+    render json: student
   end
 
 end
